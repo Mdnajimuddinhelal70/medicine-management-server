ad2f88fcdc5f1620a2ca1cafb8563b72cdd083c7
najim77288 <najimuddin77288@gmail.com>
2024-08-18 15:22:16 +0600

    added patch method for increasing and decreasing quantity

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 8000;
-
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9b7hvrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -18,66 +16,62 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-  
+
 async function run() {
   try {
-   const medicineCollection = client.db('medicinesDb').collection('medicine')
-   const cartsCollection = client.db('medicinesDb').collection('carts')
+    const medicineCollection = client.db("medicinesDb").collection("myMedicine");
+    const cartsCollection = client.db("medicinesDb").collection("carts");
 
-   app.get('/medicine', async(req, res) => {
-    const result = await medicineCollection.find().toArray();
-    res.send(result)
-   });
-
-  // carts related apis
-   app.post('/carts', async(req, res) => {
-    const cartItem = req.body;
-    const result = await cartsCollection.insertOne(cartItem)
-    res.send(result);
-   });
-   app.get('/carts', async(req, res) => {
-    const result = await cartsCollection.find().toArray();
-    res.send(result)
-   });
-   app.delete('/carts/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)};
-    const result = await cartsCollection.deleteOne(query);
-    res.send(result) 
-   });
-     
-  app.get('/medicines/category/:categoryId', async (req, res) => {
-    const categoryId = req.params.categoryId;
-  
-    try {
-      const query = { categoryId: new ObjectId(categoryId) };
-      const result = await medicineCollection.find(query).toArray();
+    app.get("/myMedicine", async (req, res) => {
+      const result = await medicineCollection.find().toArray();
       res.send(result);
-    } catch (error) {
-      console.error("Error fetching medicines:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
- 
+    });
+
+    // carts related apis
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.get("/carts", async (req, res) => {
+      const result = await cartsCollection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //Upaadte quantity
+    app.patch("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const { quantity } = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { quantity: quantity },
+      };
+      const result = await cartsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    
+
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-   
   }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('medicines project is running')
+app.get("/", (req, res) => {
+  res.send("medicines project is running");
 });
 
 app.listen(port, () => {
-    console.log(`Medicines project is running on port ${port}`)
-})
+  console.log(`Medicines project is running on port ${port}`);
+});
